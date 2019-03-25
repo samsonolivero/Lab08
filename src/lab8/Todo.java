3dcd14a48abc1f05f0da64b6b564e7d41febb4ec
Samson Olivero <samsonolivero@Samsons-MacBook-Pro.local>
2019-03-25 13:59:52 -0500

    Final Commit

---
+++ src/lab8/Todo.java
@@ -0,0 +1,32 @@
+package lab8;
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-25
+OU ID_______________: 113479362
+OU EMAIL____________: samsonolivero@ou.edu
+
+Tasks:
+1. Create Skeleton Code
+2. Develop Enum Classes
+3. Develop GamePieceAppearance Method
+4. Create the Board Game
+5. Develop A Driver Class
+6. Develop JUnit testing
+
+Estimated Time (minutes):
+1. 60
+2. 30
+3. 15
+4. 120
+5. 60
+6. 240
+
+Actual Time (minutes):
+1. 30
+2. 120
+3. 60
+4. 240
+5. 120
+6. 75
+
+*/}
